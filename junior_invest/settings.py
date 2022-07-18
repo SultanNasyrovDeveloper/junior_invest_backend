dbe2c7bb331f9e66721bd52e1248a2013f6f3ef6
SultanNasyrovDeveloper <46191469+SultanNasyrovDeveloper@users.noreply.github.com>
2022-07-18 15:54:22 +0300

    Change to static urls

--- junior_invest/settings.py
+++ junior_invest/settings.py
@@ -172,8 +172,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = pathlib.Path(BASE_DIR, 'static')
-MEDIA_URL = '/media/'
+MEDIA_URL = '/mediafiles/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
